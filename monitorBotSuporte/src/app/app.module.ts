fd761d815e238de4719a828c40e0c56aa9be674f
Thiago Chirana Macedo <96755607+othiagomacedo@users.noreply.github.com>
2023-04-22 01:35:27 -0300

    Adicionando novo front ao Bot

--- monitorBotSuporte/src/app/app.module.ts
+++ monitorBotSuporte/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/view/header/header.component';
+import { FooterComponent } from './shared/view/footer/footer.component';
+import { HomeComponent } from './shared/view/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatButtonToggleModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ monitorBotSuporte/src/app/shared/model/alerta.ts
@@ -0,0 +1,6 @@
+export interface Alerta {
+    id: number;
+    id_canal: number;
+    mensagem: number;
+    description: string;
+  }
